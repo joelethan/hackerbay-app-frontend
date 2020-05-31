0c8695c738ffcf9b61cbf9f6666836de09e2c3de
joelethan <joelkatsme@gmail.com>
2020-05-31 23:44:22 +0300

    Updates

---
+++ precache-manifest.b77ed72002cec38d742c0a7db5dd3729.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "872e51a0020259009f94",
+    "url": "/hackerbay-app-frontend/static/css/main.42e30063.chunk.css"
+  },
+  {
+    "revision": "872e51a0020259009f94",
+    "url": "/hackerbay-app-frontend/static/js/main.872e51a0.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/hackerbay-app-frontend/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "8857958bb3aa0f39553b",
+    "url": "/hackerbay-app-frontend/static/js/runtime~main.8857958b.js"
+  },
+  {
+    "revision": "db9be8904f156e87e38b9d49284b9fbe",
+    "url": "/hackerbay-app-frontend/index.html"
+  }
+];
